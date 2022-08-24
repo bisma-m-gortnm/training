42410dfbf128d3a858075d4375f9a3231dcb4db3
bisma-m-gortnm <Bisma.m@gortnm.in>
2022-08-24 17:31:15 +0530

    fun with condition

--- test.ts
+++ test.ts
@@ -1,5 +1,10 @@
-function sum(a:number,b:number){
+function sum(a:number |string ,b:number| string){
+   if(typeof a === 'number' && b ==='number'){
     return a+b;
+   }
+   else{
+     return a.toString()+b.toString()
+   }
 }
 
 console.log(sum(10,10))
