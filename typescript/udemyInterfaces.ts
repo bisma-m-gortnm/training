7d71109e256ba9f565c9b05fbb64b76156cd4a8d
bisma-m-gortnm <Bisma.m@gortnm.in>
2022-08-26 13:07:45 +0530

    Changes

--- typescript/udemyInterfaces.ts
+++ typescript/udemyInterfaces.ts
@@ -2,18 +2,20 @@ interface Trend {
     title:string,
     cost: number,
     color: string,
-    available :boolean
+    available :boolean,
+    launch : Date
 }
 const clothes ={
     title:'shirt',
     cost: 100,
     color:'red',
-    available: true
+    available: true,
+    launch : new Date()
 
 }
 
 const funCloth = (trend:Trend)=>{
-    console.log(`${trend.title}, ${trend.cost}, ${trend.color}, ${trend.available}`);
+    console.log(`${trend.title}, ${trend.cost}, ${trend.color}, ${trend.available}, ${trend.launch}`);
     
 }
 
